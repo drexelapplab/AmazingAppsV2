d6578ef87c7494c029a436f674db946aef00f5f0
Andy Stutzman <applab@drexel.edu>
2019-08-02 11:57:38 -0400

    added motion example

---
+++ Amazing AppsV2.playgroundbook/Contents/Chapters/Examples.playgroundchapter/Pages/Motion.playgroundpage/Contents.swift
@@ -0,0 +1,34 @@
+
+//: [Previous](@previous)
+//: ## Motion Example
+//:
+//: Run the code below to see an example of a touch pad of a motion controlled app.
+
+//#-hidden-code
+import PlaygroundSupport
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        let appview = UIView()
+        self.view = appview
+        //#-end-hidden-code
+        view.backgroundColor = UIColor.white
+        
+        //add a button
+        var motion = appMotion(frame: CGRect(x: 10, y: 50, width: 500, height: 500))
+        
+        motion.addMotionControl(image: "coin.png", sound1:"clap.wav", sound2:"coin.wav", delay: 60.0)
+        //set the button text
+  
+        //add the button to the screen
+        view.addSubview(motion)
+        
+        
+        //#-hidden-code
+    }
+}
+PlaygroundSupport.PlaygroundPage.current.liveView = ViewController()
+//#-end-hidden-code
+
